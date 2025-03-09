91c726f3f6f6145c830edb6a7a5c72a51f9ca7f3
Isaac Zolana <hizackdavid@hotmail.com>
2025-03-09 16:03:16 -0400

    Static Data Fetching with Revalidation

--- src/app/conference/speakers/page.tsx
+++ src/app/conference/speakers/page.tsx
@@ -12,12 +12,28 @@ type SpeakerData = {
   name: string;
 };
 
-// Static Site Generation (SSG) by default
+// Static Data Fetching or Static Site Generation (SSG) by default
 // The request will be cached until it is manually invalidated.
 // After the first request, every time this page is loaded, it pull the data from the cache and not make a request call.
+// async function fetchSpeakers() {
+//   const response = await fetch(
+//     "https://raw.githubusercontent.com/adhithiravi/Consuming-GraphqL-Apollo/master/api/data/speakers.json"
+//   );
+//   const data = await response.json();
+//   return data;
+// }
+
+// Static Data Fetching with Revalidation or Incremental Static Regeneration
+// Revalidates cache data at a specific interval (default unit: seconds).
+// - When a request is made to the route that was staticly rendered at build time,
+// it will show the cached data. The same goes for request made after the initial request and before 20 seconds.
+// - After the 20 seconds, the next request will show the cached data while next will trigger
+// a regeneration of the data in the background. If the regeneration is successful, nextjs will
+// invalidate the cached data and show the updated data. Otherwise, it will show the cached data.
 async function fetchSpeakers() {
   const response = await fetch(
-    "https://raw.githubusercontent.com/adhithiravi/Consuming-GraphqL-Apollo/master/api/data/speakers.json"
+    "https://raw.githubusercontent.com/adhithiravi/Consuming-GraphqL-Apollo/master/api/data/speakers.json",
+    { next: { revalidate: 20 } }
   );
   const data = await response.json();
   return data;
