026239ea365a9202f90a6fa394bdfca49a3c5b34
Isaac Zolana <hizackdavid@hotmail.com>
2025-03-09 15:44:54 -0400

    Static Data Fetching

--- src/app/conference/speakers/page.tsx
+++ src/app/conference/speakers/page.tsx
@@ -1,13 +1,44 @@
-import Link from "next/link";
+import styles from "../conference.module.css";
 
-const SpeakersPage = () => {
+type SpeakerSessionData = {
+  id: number;
+  name: string;
+};
+
+type SpeakerData = {
+  id: string;
+  bio: string;
+  sessions: SpeakerSessionData[];
+  name: string;
+};
+
+// Static Site Generation (SSG) by default
+// The request will be cached until it is manually invalidated.
+// After the first request, every time this page is loaded, it pull the data from the cache and not make a request call.
+async function fetchSpeakers() {
+  const response = await fetch(
+    "https://raw.githubusercontent.com/adhithiravi/Consuming-GraphqL-Apollo/master/api/data/speakers.json"
+  );
+  const data = await response.json();
+  return data;
+}
+
+const SpeakersPage = async () => {
+  const data = await fetchSpeakers();
+
   return (
-    <>
-      <h1>Welcome to Globomatics Speakers</h1>
-      <h2>
-        <Link href="/conference">Back to Conference</Link>
-      </h2>
-    </>
+    <div className={styles.parentContainer}>
+      <div className="self-start whitespace-nowrap rounded-lg bg-gray-700 px-3 py-1 text-sm font-medium tabular-nums text-gray-100">
+        Last Rendered: {new Date().toLocaleTimeString()}
+      </div>
+      <h1>Welcome to Globomantics Speakers</h1>
+      {data.speakers.map(({ id, name, bio }: SpeakerData) => (
+        <div key={id} className={styles.infoContainer}>
+          <h3 className={styles.titleText}>{name}</h3>
+          <h5 className={styles.descText}>{bio}</h5>
+        </div>
+      ))}
+    </div>
   );
 };
 
