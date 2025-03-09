1706ba96d5814eee24f8d4c4172bec32a335bdb6
Isaac Zolana <hizackdavid@hotmail.com>
2025-03-09 18:41:04 -0400

    Dynamic Data Fetching

--- src/app/conference/sessions/page.tsx
+++ src/app/conference/sessions/page.tsx
@@ -1,13 +1,75 @@
-import Link from "next/link";
+import styles from "../conference.module.css";
 
-const SessionsPage = () => {
+type SpeakerData = {
+  id: string;
+  name: string;
+};
+
+type SessionData = {
+  id: number;
+  title: string;
+  description: string;
+  startsAt: string;
+  endsAt: string;
+  speakers: SpeakerData[];
+  room: string;
+  day: string;
+  format: string;
+  track: string;
+  level: string;
+};
+
+// Dynamic Data Fetching or Server Side Rendering (SSR)
+// With this method, there is no cache.
+// The data is refreshed on every request.
+// Therefore, nextjs will not look into the cache and will not update the cache on refresh.
+async function fetchSessions() {
+  const response = await fetch(
+    "https://raw.githubusercontent.com/adhithiravi/Consuming-GraphqL-Apollo/master/api/data/sessions.json",
+    { cache: "no-store" }
+  );
+
+  const data = await response.json();
+  return data;
+}
+
+const SessionsPage = async () => {
+  const data = await fetchSessions();
+
+  console.log(data);
+
   return (
-    <>
-      <h1>Welcome to Globomatics Sessions</h1>
-      <h2>
-        <Link href="/conference">Back to Conference</Link>
-      </h2>
-    </>
+    <div className={styles.parentContainer}>
+      <div className="self-start whitespace-nowrap rounded-lg bg-gray-700 px-3 py-1 text-sm font-medium tabular-nums text-gray-100">
+        Last Rendered: {new Date().toLocaleTimeString()}
+      </div>
+      <h1>Welcome to Globomantics Sessions</h1>
+      {data.sessions.map(
+        ({
+          id,
+          title,
+          description,
+          room,
+          day,
+          track,
+          speakers,
+        }: SessionData) => (
+          <div key={id} className={styles.infoContainer}>
+            <h3 className={styles.titleText}>{title}</h3>
+            {speakers &&
+              speakers.map(({ name, id }: SpeakerData) => (
+                <h3 key={id} className={styles.titleText}>
+                  Speaker: {name}
+                </h3>
+              ))}
+            <h5 className={styles.descText}>{description}</h5>
+            <h4 className={styles.infoText}>Room: {room}</h4>
+            <h4 className={styles.infoText}>Day: {day}</h4>
+            <h4 className={styles.infoText}>Track: {track}</h4>
+          </div>
+        )
+      )}
+    </div>
   );
 };
 
